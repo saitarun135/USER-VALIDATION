a7e444d9720d7d80bc5f7bcdb891e7454696b44e
saitarun135 <saitarun135@gm.com>
2021-03-18 10:41:24 +0530

    Validating firstName with Starting caps

--- UserValidation.java
+++ UserValidation.java
@@ -1,9 +1,19 @@
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
 public class UserValidation {
-
+	public boolean validFirstName(String FirstName) {
+		String regExp="[A-Z][a-zA-Z]{3,}";
+		Pattern p = Pattern.compile(regExp);
+		Matcher m=p.matcher(FirstName);
+		return m.matches();
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcome to the USER-VALIDATION problem");
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your firstName");
+		String FirstName=sc.next();
+		boolean result=new UserValidation().validFirstName(FirstName);
+		System.out.println(result);
 	}
 
 }
