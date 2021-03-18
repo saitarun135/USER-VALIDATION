3f920b7cfc4a5e6f28fbc4e7c2ec6b04b452c6b5
saitarun135 <saitarun135@gm.com>
2021-03-18 10:50:28 +0530

    Adding lastName validation

--- UserValidation.java
+++ UserValidation.java
@@ -2,18 +2,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
 public class UserValidation {
-	public boolean validFirstName(String FirstName) {
-		String regExp="[A-Z][a-zA-Z]{3,}";
-		Pattern p = Pattern.compile(regExp);
-		Matcher m=p.matcher(FirstName);
-		return m.matches();
-	}
+	static Scanner sc = new Scanner(System.in);
+		public boolean validateFirstName(String FirstName) {
+			String regExp="[A-Z][a-zA-Z]{3,}";
+			Pattern p = Pattern.compile(regExp);
+			Matcher m=p.matcher(FirstName);
+			return m.matches();
+		}
+		public boolean validateLastName(String LastName) {
+			String regExp="[A-Z][a-zA-Z]{3,}";
+			Pattern p = Pattern.compile(regExp);
+			Matcher m=p.matcher(LastName);
+			return m.matches();
+		}
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your firstName");
 		String FirstName=sc.next();
-		boolean result=new UserValidation().validFirstName(FirstName);
+		boolean result=new UserValidation().validateFirstName(FirstName);
 		System.out.println(result);
+		System.out.println("Enter your LastName");
+		String LastName=sc.next();
+		boolean result1=new UserValidation().validateLastName(LastName);
+		System.out.println(result1);
+	
 	}
 
 }
