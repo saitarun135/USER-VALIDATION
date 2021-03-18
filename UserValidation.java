0eace6b8185e303bfab00cc5c0e379b32e646f0a
saitarun135 <saitarun135@gm.com>
2021-03-18 13:20:54 +0530

    password Strengthen(UC,LC,digit)

--- UserValidation.java
+++ UserValidation.java
@@ -16,61 +16,56 @@ static Scanner sc = new Scanner(System.in);
 		return m.matches();
 		}
 	
-	 public static void isEmailIdValid() {
-		 	System.out.println("Enter Email-Id.");
-		 	String emailId = sc.next();
-		 	if (emailId == null){
+	public static void isEmailIdValid() {
+		System.out.println("Enter Email-Id.");
+		String emailId = sc.next();
+		if (emailId == null){
 	    	 System.out.println("Empty Field.");
 		      }
-		 	else{
-		 	if(emailId.matches("^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-z0-9]+.[a-zA-Z]{2}([.][a-zA-Z]{2,4}){0,1}$")) {
+		else{
+		if(emailId.matches("^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-z0-9]+.[a-zA-Z]{2}([.][a-zA-Z]{2,4}){0,1}$")) {
 		 		System.out.println("E-mail Id is Valid");
 		 		}
-		 	else {
+		else {
 		 		System.out.println("E-mail Id is Invalid");
 		        }
 		    }
 	   }
-	 public boolean validateMobile(String mobileNumber) {
-		 	String regExp="^[0-9]{2}[-][0-9]{10}$";
-			Pattern p = Pattern.compile(regExp);
-			Matcher m=p.matcher(mobileNumber);
-			return m.matches();
+	public boolean validateMobile(String mobileNumber) {
+		String regExp="^[0-9]{2}[-][0-9]{10}$";
+		Pattern p = Pattern.compile(regExp);
+		Matcher m=p.matcher(mobileNumber);
+		return m.matches();
 		}
-	 public static void isPasswordValidation() {
-		 System.out.println("Enter a password:");
-		 String password=sc.next();
-		 boolean upperCase=false;
-		 char[] pwArray=password.toCharArray();
-		 for(int i=0;i<pwArray.length;i++) {
-			 if(Character.isUpperCase(pwArray[i])) {
-				 upperCase=true;
-			 }
-			if(pwArray.length>=8 &&upperCase) {
-				System.out.println("Entered password is valid...");
-				break;
-			}
-			else {
-				System.out.println("Entered password is invalid");
-				break;
-			}
-		 }
-	 	}
+	public static boolean isPasswordValidation(String password) {
+		String regExp="^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}$";
+		Pattern p = Pattern.compile(regExp);
+		Matcher m=p.matcher(password);
+		return m.matches();
+	 }
 
 public static void main(String[] args) {
 	System.out.println("Enter your firstName");
 	String FirstName=sc.next();
 	boolean result=new UserValidation().validateFirstName(FirstName);
 	System.out.println(result);
+	
 	System.out.println("Enter your LastName");
 	String LastName=sc.next();
 	boolean result1=new UserValidation().validateLastName(LastName);
 	System.out.println(result1);
+	
 	isEmailIdValid();
+	
 	System.out.println("Enter Mobile-Number:");
 	String mobileNumber=sc.next();
 	boolean result3=new UserValidation().validateMobile(mobileNumber);
 	System.out.println(result3);
-	isPasswordValidation();
+	
+	System.out.println("Enter your password(uppercase,lowercase,digit):");
+	String password=sc.next();
+	boolean result4=new UserValidation().isPasswordValidation(password);
+	System.out.println(result4);
+	
 }
 }
